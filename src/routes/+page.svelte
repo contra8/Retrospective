89b808ed37953d901649457afda9d0db1eba1a35
contra8 <christian.achter@gmail.com>
2023-05-07 11:59:53 +0200

    Adjust indentation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
-  import Card from './Card.svelte';
+  	import Card from './Card.svelte';
 
 	let inputArea;
 	let startContainer;
